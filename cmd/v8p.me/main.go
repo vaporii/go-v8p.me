915f88605171fccbbbdd6aea45e426f856cc7dad
vaporii <124741890+vaporii@users.noreply.github.com>
2025-07-25 15:31:30 +0000

    set up chi router

--- cmd/v8p.me/main.go
+++ cmd/v8p.me/main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/vaporii/go-v8p.me/internal/config"
+)
 
 func main() {
-	fmt.Println("Hello, world!")
+	conf := config.LoadConfig()
+
+	r := chi.NewRouter()
+
+	log.Println(http.ListenAndServe(conf.ServerAddress, r))
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/vaporii/go-v8p.me
 
 go 1.24.5
+
+require github.com/go-chi/chi/v5 v5.2.2
